ad4b86d8ba7e0e238841b2ed5850cc48abe201a4
sabbir073 <md.sabbir073@gmail.com>
2025-12-07 17:03:57 +0600

    done

--- components/MobileMenu/index.js
+++ components/MobileMenu/index.js
@@ -1,163 +1,36 @@
-import React, { Fragment, useState } from 'react';
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import Collapse from "@mui/material/Collapse";
+import React, { useState } from 'react';
 import Link from "next/link";
+import Image from 'next/image';
+import Logo from '/public/images/logo.png';
 
 const menus = [
     {
         id: 1,
         title: 'Home',
-        link: '/home',
-        submenu: [
-            {
-                id: 11,
-                title: 'Home style 1',
-                link: '/home'
-            },
-            {
-                id: 12,
-                title: 'Home style 2',
-                link: '/home2'
-            },
-            {
-                id: 13,
-                title: 'Home style 3',
-                link: '/home3'
-            },
-        ]
+        link: '/',
     },
-
     {
         id: 2,
         title: 'About',
         link: '/about',
     },
-
-{
-    id: 3,
+    {
+        id: 3,
         title: 'Services',
         link: '/service',
-        submenu: [
-            {
-                id: 31,
-                title: 'Service',
-                link: '/service'
-            },
-            {
-                id: 32,
-                title: 'Service style 2',
-                link: '/service-s2'
-            },
-            {
-                id: 33,
-                title: 'Service Single',
-                link: '/service-single/Strategy-and-Planning'
-            }
-        ]
     },
-{
-    id: 4,
+    {
+        id: 4,
         title: 'Projects',
         link: '/project',
-        submenu: [
-            {
-                id: 41,
-                title: 'Projects',
-                link: '/project'
-            },
-            {
-                id: 42,
-                title: 'Projects style 2',
-                link: '/project-s2'
-            },
-            {
-                id: 43,
-                title: 'Projects Single',
-                link: '/project-single/Consumer-Markets'
-            }
-        ]
     },
-
-    {
-        id: 7,
-        title: 'Pages',
-        link: '/',
-        submenu: [
-            {
-                id: 71,
-                title: 'Pricing',
-                link: '/pricing'
-            },
-            {
-                id: 75,
-                title: 'Testimonial',
-                link: '/testimonial'
-            },
-            {
-                id: 76,
-                title: 'Error 404',
-                link: '/404'
-            },
-    
-            {
-                id: 77,
-                title: 'Login',
-                link: '/login'
-            },
-            {
-                id: 78,
-                title: 'Register',
-                link: '/register'
-            },
-            
-        ]
-    },
-
     {
         id: 5,
-        title: 'Blog',
-        link: '/blog',
-        submenu: [
-            {
-                id: 51,
-                title: 'Blog',
-                link: '/blog'
-            },
-            {
-                id: 52,
-                title: 'Blog Left sidebar',
-                link: '/blog-left'
-            },
-            {
-                id: 53,
-                title: 'Blog full width',
-                link: '/blog-fullwidth'
-            },
-            {
-                id: 54,
-                title: 'Blog single',
-                link: '/blog-single/8-Mistakes-First-Time-Founders-Make-When-Starting-a-Business"'
-            },
-            {
-                id: 55,
-                title: 'Blog single Left sidebar',
-                link: '/blog-single-left-sidebar/8-Mistakes-First-Time-Founders-Make-When-Starting-a-Business'
-            },
-            {
-                id: 56,
-                title: 'Blog single Left sidebar',
-                link: '/blog-single-fullwidth/8-Mistakes-First-Time-Founders-Make-When-Starting-a-Business'
-            },
-        ]
-    },
-    {
-        id: 89,
         title: 'Seafarer Jobs',
         link: '/seafarer-jobs',
     },
     {
-        id: 88,
+        id: 6,
         title: 'Contact',
         link: '/contact',
     }
@@ -166,54 +39,63 @@ const menus = [
 
 const MobileMenu = () => {
 
-    const [openId, setOpenId] = useState(0);
     const [menuActive, setMenuState] = useState(false);
 
     const ClickHandler = () => {
         window.scrollTo(10, 0);
+        setMenuState(false);
     }
 
     return (
         <div>
+            {/* Overlay */}
+            <div
+                className={`mobile-menu-overlay ${menuActive ? "active" : ""}`}
+                onClick={() => setMenuState(false)}
+            />
+
+            {/* Mobile Menu */}
             <div className={`mobileMenu ${menuActive ? "show" : ""}`}>
-                <div className="menu-close">
-                    <div className="clox" onClick={() => setMenuState(!menuActive)}><i className="ti-close"></i></div>
+                <div className="mobile-menu-header">
+                    <div className="mobile-logo">
+                        <Link href="/" onClick={ClickHandler}>
+                            <Image src={Logo} alt="RBM Maritime" width={120} height={40} />
+                        </Link>
+                    </div>
+                    <div className="close-btn" onClick={() => setMenuState(!menuActive)}>
+                        <i className="ti-close"></i>
+                    </div>
                 </div>
 
-                <ul className="responsivemenu">
-                    {menus.map((item, mn) => {
-                        return (
-                            <ListItem className={item.id === openId ? 'active' : null}  key={mn}>
-                                {item.submenu ?
-                                    <Fragment>
-                                        <p onClick={() => setOpenId(item.id === openId ? 0 : item.id)}>{item.title}
-                                          <i className={item.id === openId ? 'fa fa-angle-up' : 'fa fa-angle-down'}></i>
-                                        </p>
-                                        <Collapse in={item.id === openId} timeout="auto" unmountOnExit>
-                                            <List className="subMenu">
-                                                <Fragment>
-                                                    {item.submenu.map((submenu, i) => {
-                                                        return (
-                                                            <ListItem key={i}>
-                                                                <Link onClick={ClickHandler} className="active"
-                                                                    href={submenu.link}>{submenu.title}</Link>
-                                                            </ListItem>
-                                                        )
-                                                    })}
-                                                </Fragment>
-                                            </List>
-                                        </Collapse>
-                                    </Fragment>
-                                    : <Link className='active'
-                                        href={item.link}>{item.title}</Link>
-                                }
-                            </ListItem>
-                        )
-                    })}
-                </ul>
+                <nav className="mobile-nav">
+                    <ul>
+                        {menus.map((item, index) => (
+                            <li key={index}>
+                                <Link href={item.link} onClick={ClickHandler}>
+                                    {item.title}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
 
+                <div className="mobile-menu-footer">
+                    <div className="contact-info">
+                        <p><i className="ti-mobile"></i> +880 1818-130878</p>
+                        <p><i className="ti-email"></i> info@rbmbd.com</p>
+                    </div>
+                    <div className="social-links">
+                        <Link href="https://www.facebook.com/rbmmaritime" target="_blank">
+                            <i className="ti-facebook"></i>
+                        </Link>
+                        <Link href="https://wa.me/8801818130878" target="_blank">
+                            <i className="fa fa-whatsapp"></i>
+                        </Link>
+                    </div>
+                </div>
             </div>
 
+            {/* Hamburger Button */}
             <div className="showmenu" onClick={() => setMenuState(!menuActive)}>
                 <button type="button" className="navbar-toggler open-btn">
                     <span className="icon-bar first-angle"></span>
--- components/hero3/index.js
+++ components/hero3/index.js
@@ -1,41 +1,26 @@
-import React, { Component } from "react";
-import Slider from "react-slick";
+import React from "react";
 import Link from 'next/link'
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-
+import Image from 'next/image'
+import heroImg from '/public/images/rbm.jpg'
 
 
 const Hero3 = () => {
 
-    var settings = {
-        dots: false,
-        arrows: true,
-        speed: 1200,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2500,
-        fade: true
-    };
-
     return (
         <section className="wpo-hero-slider">
             <div className="hero-container">
-                <div className="hero-wrapper">
-                    <Slider {...settings}>
-                        <div className="hero-slide">
-                            <div className="slide-inner slide-bg-image" style={{ backgroundImage: `url(${'images/rbm.jpg'})` }}>
-                                <div className="container">
-                                    <div className="slide-content">
-                                        <div className="slide-btns">
-                                            <Link href="/contact" className="theme-btn">Get Started</Link>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                <div className="hero-slide">
+                    <div className="slide-inner">
+                        <Image
+                            src={heroImg}
+                            alt="RBM Maritime"
+                            style={{ width: '100%', height: 'auto' }}
+                            priority
+                        />
+                        <div className="slide-content-overlay">
+                            <Link href="/contact" className="theme-btn">Get Started</Link>
                         </div>
-                    </Slider>
+                    </div>
                 </div>
             </div>
         </section>
